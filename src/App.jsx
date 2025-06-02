0ecd1c571d9fdec8a2af395c99bd91f8bddfafc8
subhash mhetre <subhashmhetre369@gmail.com>
2025-06-02 19:32:12 +0530

    my-frist-app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './app.css'
+import Login from './Components/auth/login'
+
+function App() {
+    return (
+        <div>
+        
+
+            Subhash vaishnavi
+        </div>
+    )
+}
+
+export default App
+
